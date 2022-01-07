77bb856311890d9d6ae5f43c2b7d9a08287a16e3
grafaelferreira <getulio.ferreira@datum.inf.br>
2022-01-07 17:16:52 -0300

    feat: Styling components

--- src/components/Cell.js
+++ src/components/Cell.js
@@ -1,11 +1,9 @@
-import React from 'react'
+import React from 'react';
+import { StyledCell } from './styles/StyledCell';
+import { TETROMINOS } from '../tetrominos';
 
-function Cell({ type }) {
-    return (
-        <div>
-            CELL
-        </div>
-    )
-}
+const Cell = ({ type }) => (
+  <StyledCell type={type} color={TETROMINOS[type].color} />
+)
 
-export default Cell
+export default Cell;
